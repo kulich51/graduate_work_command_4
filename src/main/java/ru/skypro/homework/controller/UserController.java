7791f0abe75037187b44cbb1af128b543ae1cc2a
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-07-31 12:38:06 +0300

    Добавлен метод addNewUser

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -2,12 +2,32 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.RegisterReq;
+import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.service.AuthService;
+
+import static ru.skypro.homework.dto.Role.USER;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
+@RequestMapping("users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final AuthService authService;
+
+    @PostMapping
+    ResponseEntity<?> addNewUser(@RequestBody RegisterReq user) {
+
+        Role role = user.getRole() == null ? USER : user.getRole();
+        if (authService.register(user, role)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
