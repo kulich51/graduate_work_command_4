fb264afe7fa17fc150710f2b7f3c913a0bad5500
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-08-02 21:41:03 +0300

    Добавлены таблицы users и authorities

--- src/main/java/ru/skypro/homework/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/WebSecurityConfig.java
@@ -1,17 +1,30 @@
 package ru.skypro.homework;
 
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfig {
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String databaseUser;
+
+    @Value("${spring.datasource.password}")
+    private String databaseUserPassword;
 
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
@@ -32,6 +45,20 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    @Primary
+    public JdbcUserDetailsManager jdbcDetailsService() {
+
+        PGSimpleDataSource dbSource = new PGSimpleDataSource();
+
+        // Смотри документацию к методу setServerNames. Если serverNames == null, то по умолчанию сервер localhost
+        dbSource.setServerNames(null);
+        dbSource.setDatabaseName(getDbName(databaseUrl));
+        dbSource.setUser(databaseUser);
+        dbSource.setPassword(databaseUserPassword);
+        return new JdbcUserDetailsManager(dbSource);
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
@@ -46,6 +73,10 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    private String getDbName(String datasource) {
+
+        return datasource.substring(datasource.lastIndexOf("/") + 1);
+    }
 
 }
 
