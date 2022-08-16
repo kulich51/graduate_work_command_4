b850375841243988eba481671434a7a2fcd94c6f
Samantha <sammy23051996@gmail.com>
2022-08-16 19:36:54 +0300

    Добавили сортировку

--- src/main/java/ru/skypro/homework/repository/AdsRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsRepository.java
@@ -13,5 +13,5 @@ import java.util.Optional;
 public interface AdsRepository extends JpaRepository<Ads, Long> {
 
     void deleteAllById(Long adsId);
-    Collection<Ads> findByTitleContains(String title);
+    Collection<Ads> findByTitleContainsOrderByTitle(String title);
 }
