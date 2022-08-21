f823aedeb749fea6c7c4294b690e4a7abae8a0ef
Samantha <sammy23051996@gmail.com>
2022-08-21 19:12:32 +0300

    Добавлены разгронечители  доступа для AdsController

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.*;
 import ru.skypro.homework.service.AdsService;
@@ -21,22 +22,25 @@ public class AdsController {
 
     @GetMapping
     ResponseEntity<ResponseWrapper<AdsDto>> getAllAds(@RequestParam(required = false) String title) {
-       ResponseWrapper<AdsDto> ads = new ResponseWrapper<>(adsService.getAds(title));
-       return ResponseEntity.ok(ads);
+        ResponseWrapper<AdsDto> ads = new ResponseWrapper<>(adsService.getAds(title));
+        return ResponseEntity.ok(ads);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PostMapping
     ResponseEntity<AdsDto> createAds(@RequestBody CreateAds ads) {
 
         return ResponseEntity.ok(adsService.save(ads));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("me")
     ResponseEntity<ResponseWrapper<AdsDto>> getAdsMe(@RequestParam(required = false) String title) {
         ResponseWrapper<AdsDto> ads = new ResponseWrapper<>(adsService.getAds(title));
         return ResponseEntity.ok(ads);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("{ad_pk}/comment")
     ResponseEntity<ResponseWrapper<AdsComment>> getAdsComments(@PathVariable(value = "ad_pk") Long adsId) {
 
@@ -44,6 +48,7 @@ public class AdsController {
         return ResponseEntity.ok(ads);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PostMapping("{ad_pk}/comment")
     ResponseEntity<AdsComment> addAdsComment(@PathVariable(value = "ad_pk") Long adsId,
                                              @RequestBody AdsComment comment) {
@@ -52,6 +57,7 @@ public class AdsController {
         return ResponseEntity.ok(adsService.addComment(adsId, comment));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @DeleteMapping("/ads/{ad_pk}/comment/{id}")
     ResponseEntity<?> deleteAdsComment(@PathVariable(value = "ad_pk") Long adsId,
                                        @PathVariable(value = "id") Long commentId) {
@@ -59,6 +65,7 @@ public class AdsController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/ads/{ad_pk}/comment/{id}")
     ResponseEntity<AdsComment> getAdsComment(@PathVariable(value = "ad_pk") Long adsId,
                                              @PathVariable(value = "id") Long commentId) {
@@ -66,6 +73,7 @@ public class AdsController {
         return ResponseEntity.ok(adsService.getAdsComment(adsId, commentId));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PatchMapping("/ads/{ad_pk}/comment/{id}")
     ResponseEntity<AdsComment> updateAdsComment(@PathVariable(value = "ad_pk") Long adsId,
                                                 @PathVariable(value = "id") Long commentId,
@@ -73,6 +81,7 @@ public class AdsController {
         return ResponseEntity.ok(adsService.updateAdsComment(adsId, commentId, adsComment));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @DeleteMapping("/ads/{id}")
     ResponseEntity<?> removeAds(@PathVariable Long id) {
 
@@ -80,12 +89,14 @@ public class AdsController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping("/ads/{id}")
     ResponseEntity<FullAdsDto> getFullAds(@PathVariable Long id) {
 
         return ResponseEntity.ok(adsService.getFullAds(id));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PatchMapping("/ads/{id}")
     ResponseEntity<AdsDto> updateAds(@PathVariable Long id,
                                      @RequestBody AdsDto updatedAds) {
