6ef6e348c4ed199bccee88feb996c1772fc1f53a
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-08-28 18:13:54 +0300

    Оптимизированы импорты

--- src/main/java/ru/skypro/homework/controller/ImageController.java
+++ src/main/java/ru/skypro/homework/controller/ImageController.java
@@ -2,8 +2,10 @@ package ru.skypro.homework.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.homework.service.AdsService;
 
 @Slf4j
