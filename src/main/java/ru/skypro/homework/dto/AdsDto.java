ecd3d7834d6c7e6a8ad7b12702f31c5b93a84d3b
kulich51 <59617932+kulich51@users.noreply.github.com>
2022-08-29 21:38:53 +0300

    Картинки видны на фронте

--- src/main/java/ru/skypro/homework/dto/AdsDto.java
+++ src/main/java/ru/skypro/homework/dto/AdsDto.java
@@ -8,7 +8,7 @@ public class AdsDto {
     private Long pk;
     private final Long author;
     private final String title;
-    private final String image;
+    private String image;
     private final int price;
     private String description;
 }
--- src/main/java/ru/skypro/homework/mapper/AdsMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdsMapper.java
@@ -1,7 +1,6 @@
 package ru.skypro.homework.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 import ru.skypro.homework.dto.AdsDto;
 import ru.skypro.homework.dto.CreateAds;
@@ -9,7 +8,7 @@ import ru.skypro.homework.entity.Ads;
 
 import java.util.Collection;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AdsMapper {
 
     AdsMapper INSTANCE = Mappers.getMapper(AdsMapper.class);
@@ -28,4 +27,10 @@ public interface AdsMapper {
     Ads adsDtoToAds(AdsDto ads);
 
     Collection<AdsDto> adsCollectionToAdsDto(Collection<Ads> adsCollection);
+
+    @AfterMapping
+    default void addSlash(@MappingTarget AdsDto adsDto) {
+        String image = adsDto.getImage();
+        adsDto.setImage("/" + image);
+    }
 }
